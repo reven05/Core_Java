70314bdc3bdf9825b3c23ef4ef7ec4d765f46e70
TNSIF India <TNSIF India@REVEN-PC>
2023-03-29 17:37:36 +0530

    First commit

---
+++ C9/src/day2/GitDemo.java
@@ -0,0 +1,9 @@
+package day2;
+
+public class GitDemo {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ C9/src/day_1/Introduction.java
@@ -0,0 +1,11 @@
+package day_1;
+
+public class Introduction {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome Guys");
+		System.out.println("Nice to meet you all!!!");
+		System.out.println("Introduction.main()");
+	}
+
+}
