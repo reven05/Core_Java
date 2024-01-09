10df9a255edede5403d4d0808bdfb9ed6a8909b0
TNSIF India <TNSIF India@REVEN-TNS>
2024-01-09 10:38:08 +0530

    dwdfvsg

---
+++ Sample/src/com/cg/dayone/SampleClass.java
@@ -0,0 +1,10 @@
+package com.cg.dayone;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, Hello!!!");
+	}
+
+}
